9a328ef9b467a02455050f7bf6e416735dde887e
Romain <romain.boudot@epsi.fr>
2018-05-15 20:44:19 +0200

    maketeu

---
+++ html/maketeu/consultant.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <title>Profile Consultant</title>
+</head>
+<body>
+    <header>
+        <div class="header-left">
+            <img id="logo-a2f" src="/images/logo-a2f-blanc-02.svg" height="46">
+        </div>
+        <div class="header-right">
+            <div class="btn bold">Déconnexion</div>
+        </div>
+        <div class="header-right">
+            <div>Bienvenue, <span>romain.boudot</span></div>
+        </div>
+    </header>
+    <nav>
+        <div id="image-profile">
+            <img src="/images/unknown.png" alt="profile image">
+        </div>
+        <div class="hr"></div>
+        <div class="profile-info" data-info="prenom">Romain</div>
+        <div class="profile-info" data-info="nom">Boudot</div>
+        <div class="profile-info" data-info="email">romain.boudot@epsi.fr</div>
+        <div class="profile-info" data-info="linkedin">monlienverslinkedin</div>
+        <div class="bottom btn w-100 h-56 modif-profile bold">Modifier mon profil</div>
+    </nav>
+
+    <div class="main-wrapper">
+        
+    </div>
+
+</body>
+</html>
