5335ab22fe3c076d33c17f09877698efddd60309
Unknown <romain.boudot@epsi.fr>
2018-05-16 21:32:17 +0200

    c'est beauuuuuuuuu

--- html/cdn/Dropdown.js
+++ html/cdn/Dropdown.js
@@ -7,13 +7,13 @@ class Dropdown {
         this.trigger = trigger;
         this.target = target;
         this.status = status;
+        this.toHide = [];
 
         if (this.status == true) {
-            this.target.style.height = this.target.scrollHeight + "px";
-            this.trigger.style.backgroundColor = "rgba(0, 0, 0, 0.144)";
+            this.show();
         } else {
-            this.target.style.height = "0px";
-            this.trigger.style.backgroundColor = "unset";
+            this.hideText();
+            this.hideDiv();
         }
 
     }
@@ -22,25 +22,69 @@ class Dropdown {
         this.toHide = toHide;
     }
 
+    set statusVal(status) {
+        this.status = status;
+    }
+
+    get statusVal() {
+        return this.status;
+    }
+
     switch_status() {
         if (this.status == true) {
-            this.status = false;
-            this.target.style.height = "0px";
-            this.trigger.style.backgroundColor = "unset";
+            this.hide();
         } else if (this.status == false) {
-            this.status = true;
-            this.target.style.height = this.target.scrollHeight + "px";
-            this.trigger.style.backgroundColor = "rgba(0, 0, 0, 0.144)";
-            this.toHide.forEach(elem => {
-                elem.hide();
-            });
+            this.show();
         }
+    }
+
+    hideText() {
+        this.target.style.color = "rgba(0, 0, 0, 0)";
+    }
+
+    hideDiv() {
+        this.target.style.height = "0px";
+    }
+
+    hideColor() {
+        this.trigger.style.backgroundColor = "unset";
     }
 
     hide() {
         this.status = false;
-        this.target.style.height = "0px";
         this.trigger.style.backgroundColor = "unset";
+        this.target.style.color = "rgba(0, 0, 0, 0)";
+        setTimeout(() => {
+            this.target.style.height = "0px";
+        }, 200);
+    }
+
+    show() {
+        this.status = true;
+        this.trigger.style.backgroundColor = "rgba(0, 0, 0, 0.144)";
+        let check = false;
+        let timeout = 0;
+        this.toHide.forEach(elem => {
+            if (elem.statusVal) check++;
+        });
+        if (check > 0) {
+            this.toHide.forEach(elem => {
+                elem.hideText();
+                elem.hideColor();
+                elem.statusVal = false;
+            });
+            timeout = 200;
+        }
+        setTimeout(() => {
+            this.target.style.height = this.target.scrollHeight + "px";
+            this.toHide.forEach(elem => {
+                elem.hideDiv();
+            });
+            setTimeout(() => {
+                this.target.style.color = "inherit";
+            }, 300);
+        }, timeout);
+
     }
 
 }
--- html/maketeu/consultant.php
+++ html/maketeu/consultant.php
@@ -32,99 +32,159 @@
         <div class="profile-info" data-info="nom">Boudot</div>
         <div class="profile-info" data-info="email">romain.boudot@epsi.fr</div>
         <div class="profile-info" data-info="linkedin">monlienverslinkedin</div>
-        <div class="bottom btn w-100 h-56 modif-profile bold">Modifier mon profil</div>
+        <div class="bottom btn h-56 modif-profile bold">Modifier mon profil</div>
     </nav>
 
     <div class="main-wrapper">
-        <div id="onglets-wrapper">
-            <div id="ongletInt" class="onglet-label">Interventions</div><div id="ongletQual" class="onglet-label">Qualifications</div><div id="ongletComp" class="onglet-label">Compétences</div>
-            <div id="ongletIntContent" class="onglet">
-                <ul>
-                    <li>coucou</li>
-                    <li>test</li>
-                    <li>another coucou</li>
-                </ul>
+        <div class="relative-wrapper-container">
+            <div style="width: 100%; margin-bottom: 20px; position: relative; height: 0px;overflow: hidden;">fixing div</div>
+            <div id="onglets-wrapper">
+                <div id="ongletInt" class="onglet-label">Interventions</div><div id="ongletQual" class="onglet-label">Qualifications</div><div id="ongletComp" class="onglet-label">Compétences</div>
+                <div id="ongletIntContent" class="onglet">
+                    <div class="intervention">
+                        <div class="infos"><span>17 mai 2018</span><span>A2F advisor</span></div>
+                        <div class="details">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+                        </div>
+                    </div>
+                    <div class="intervention">
+                        <div class="infos"><span>17 mai 2018</span><span>A2F advisor</span></div>
+                        <div class="details">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
+                        </div>
+                    </div>
+                </div>
+                <div id="ongletQualContent" class="onglet">
+                    <ul>
+                        <li>DES QUALIFICATION DE QUALITEY</li>
+                        <li>en elevage de bovin</li>
+                        <li>peche a la ligne</li>
+                        <li>chasse a l'autruche</li>
+                    </ul>
+                </div>
+                <div id="ongletCompContent" class="onglet">
+                    <ul>
+                        <li>comp 1</li>
+                        <li>comp 2</li>
+                        <li>comp 3</li>
+                        <li>comp 4</li>
+                        <li>comp 1</li>
+                        <li>comp 2</li>
+                        <li>comp 3</li>
+                        <li>comp 4</li>
+                        <li>comp 1</li>
+                        <li>comp 2</li>
+                        <li>comp 3</li>
+                        <li>comp 4</li>
+                        <li>comp 1</li>
+                        <li>comp 2</li>
+                        <li>comp 3</li>
+                        <li>comp 4</li>
+                        <li>comp 1</li>
+                        <li>comp 2</li>
+                        <li>comp 3</li>
+                        <li>comp 4</li>
+                        <li>comp 1</li>
+                        <li>comp 2</li>
+                        <li>comp 3</li>
+                        <li>comp 4</li>
+                        <li>comp 1</li>
+                        <li>comp 2</li>
+                        <li>comp 3</li>
+                        <li>comp 4</li>
+                        <li>comp 1</li>
+                        <li>comp 2</li>
+                        <li>comp 3</li>
+                        <li>comp 4</li>
+                        <li>comp 1</li>
+                        <li>comp 2</li>
+                        <li>comp 3</li>
+                        <li>comp 4</li>
+                        <li>comp 1</li>
+                        <li>comp 2</li>
+                        <li>comp 3</li>
+                        <li>comp 4</li>
+                        <li>comp 1</li>
+                        <li>comp 2</li>
+                        <li>comp 3</li>
+                        <li>comp 4</li>
+                    </ul>
+                </div>
+
+                <script>
+                    
+                    var ongletInt = new Dropdown(
+                        document.getElementById("ongletInt"),
+                        document.getElementById("ongletIntContent"),
+                        false
+                    );
+
+                    var ongletQual = new Dropdown(
+                        document.getElementById("ongletQual"),
+                        document.getElementById("ongletQualContent"),
+                        false
+                    );
+
+                    var ongletComp = new Dropdown(
+                        document.getElementById("ongletComp"),
+                        document.getElementById("ongletCompContent"),
+                        false
+                    );
+
+                    ongletInt.trigger.addEventListener("click", function() {
+                        ongletInt.switch_status();
+                    });
+
+                    ongletQual.trigger.addEventListener("click", function() {
+                        ongletQual.switch_status();
+                    });
+
+                    ongletComp.trigger.addEventListener("click", function() {
+                        ongletComp.switch_status();
+                    });
+
+                    ongletInt.toHideElem = [ongletQual, ongletComp];
+                    ongletQual.toHideElem = [ongletInt, ongletComp];
+                    ongletComp.toHideElem = [ongletInt, ongletQual];
+
+                </script>
+
             </div>
-            <div id="ongletQualContent" class="onglet">
-                <ul>
-                    <li>DES QUALIFICATION DE QUALITEY</li>
-                    <li>en elevage de bovin</li>
-                    <li>peche a la ligne</li>
-                    <li>chasse a l'autruche</li>
-                </ul>
+            <div id="chart-wrapper">
+                <canvas id="chart-p1" class="chartjs" width="200" height="200"></canvas>
+                <script>
+                    var co1 = new chartOption();
+                    co1.chart.data.datasets[0].data = [3,2,3]
+                    co1.chart.data.labels = ["bonjour","hola","hello"]
+                    new Chart(document.getElementById("chart-p1"), co1.option);
+                </script>
+                <canvas id="chart-p2" class="chartjs" width="200" height="200"></canvas>
+                <script>
+                    var co2 = new chartOption();
+                    co2.chart.data.datasets[0].data = [1,2,1]
+                    co2.chart.data.labels = ["bonjour","hola","hello"]
+                    new Chart(document.getElementById("chart-p2"), co2.option);
+                </script>
+                <canvas id="chart-p3" class="chartjs" width="200" height="200"></canvas>
+                <script>
+                    var co3 = new chartOption();
+                    co3.chart.data.datasets[0].data = [3,2,1]
+                    co3.chart.data.labels = ["bonjour","hola","hello"]
+                    new Chart(document.getElementById("chart-p3"), co3.option);
+                </script>
             </div>
-            <div id="ongletCompContent" class="onglet">
-                <ul>
-                    <li>comp 1</li>
-                    <li>comp 2</li>
-                    <li>comp 3</li>
-                </ul>
-            </div>
-
-            <script>
-                
-                var ongletInt = new Dropdown(
-                    document.getElementById("ongletInt"),
-                    document.getElementById("ongletIntContent"),
-                    false
-                );
-
-                var ongletQual = new Dropdown(
-                    document.getElementById("ongletQual"),
-                    document.getElementById("ongletQualContent"),
-                    false
-                );
-
-                var ongletComp = new Dropdown(
-                    document.getElementById("ongletComp"),
-                    document.getElementById("ongletCompContent"),
-                    false
-                );
-
-                ongletInt.trigger.addEventListener("click", function() {
-                    ongletInt.switch_status();
-                });
-
-                ongletQual.trigger.addEventListener("click", function() {
-                    ongletQual.switch_status();
-                });
-
-                ongletComp.trigger.addEventListener("click", function() {
-                    ongletComp.switch_status();
-                });
-
-                ongletInt.toHideElem = [ongletQual, ongletComp];
-                ongletQual.toHideElem = [ongletInt, ongletComp];
-                ongletComp.toHideElem = [ongletInt, ongletQual];
-
-            </script>
-
-        </div>
-        <div id="chart-wrapper">
-            <canvas id="chart-p1" class="chartjs" width="200" height="200"></canvas>
-            <script>
-                var co1 = new chartOption();
-                co1.chart.data.datasets[0].data = [3,2,3]
-                co1.chart.data.labels = ["bonjour","hola","hello"]
-                new Chart(document.getElementById("chart-p1"), co1.option);
-            </script>
-            <canvas id="chart-p2" class="chartjs" width="200" height="200"></canvas>
-            <script>
-                var co2 = new chartOption();
-                co2.chart.data.datasets[0].data = [1,2,1]
-                co2.chart.data.labels = ["bonjour","hola","hello"]
-                new Chart(document.getElementById("chart-p2"), co2.option);
-            </script>
-            <canvas id="chart-p3" class="chartjs" width="200" height="200"></canvas>
-            <script>
-                var co3 = new chartOption();
-                co3.chart.data.datasets[0].data = [3,2,1]
-                co3.chart.data.labels = ["bonjour","hola","hello"]
-                new Chart(document.getElementById("chart-p3"), co3.option);
-            </script>
-        </div>
-        <div id="timeLine">
-
+            <div id="timeLine">
+                <div class="line">
+                    <div class="point">
+                        <div class="tooltip">
+                            Arriver dans l'entreprise
+                        </div>
+                        <div class="pointLabel">
+                            14 mai 2018
+                        </div>
+                    </div>
+                </div>
+            </div>         
         </div>
     </div>
 
