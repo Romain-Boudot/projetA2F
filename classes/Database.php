4a9cc5d105841eddbff9fa8054d98e3da8f4bb1a
Romain <romain.boudot@epsi.fr>
2018-05-18 09:18:09 +0200

    Database.php class fix

--- classes/Database.php
+++ classes/Database.php
@@ -1,49 +1,72 @@
 <?php 
 
-class Database
-{
-    //connection à la BDD
+class Database {
+
+    //connection a la BDD
 
     public function connect() {
+
         try {
+
             return new PDO('mysql:host=localhost;dbname=projetA2F', 'userA2F', 'A2FBDD@2018!');
+
         } catch (Exception $e) {
+
             return false;
+
         }
+    
     }
 
     public function login($type, $login, $password) {
-        if ($type = 0) {
-            $table = "consultant";
-        } elseif ($type = 1) {
-            $table = "BM";
-        } elseif ($type = 2) {
-            $table = "RH";
-        }
-    }
 
-    $db = Database::connect()
+        if ($type = 0) {
+
+            $table = "consultant";
+
+        } elseif ($type = 1) {
+
+            $table = "BM";
+
+        } elseif ($type = 2) {
+
+            $table = "RH";
+
+        }
+
+        $db = Database::connect()
 
         if ($db == false) return false;
 
-    $statement = $db->prepare("SELECT * FROM " . $table . "where login = :login");
-    $statement->execute(array(
-        ":login" => $login
-    ));
+        $statement = $db->prepare("SELECT * FROM " . $table . "where login = :login");
+        $statement->execute(array(
+            ":login" => $login
+        ));
 
-    $answer = $statement->fetch();
+        $answer = $statement->fetch();
 
 
-    // vérification du identifiant + mdp
+        // verification du identifiant + mdp
 
-    if ($answer == false) {
-        return false;
-    } else {
-        if ($answer['mot_de_passe'] == $password) {
-            return true;
-        } else {
+        if ($answer == false) {
+    
             return false;
+    
+        } else {
+    
+            if ($answer['mot_de_passe'] == $password) {
+    
+                return true;
+    
+            } else {
+    
+                return false;
+    
+            }
+    
         }
+    
     }
+
 }
 
