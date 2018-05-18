47ae5384b0e9b30cbc78682e3b5efb67a2ee840d
Romain <romain.boudot@epsi.fr>
2018-05-18 12:12:19 +0200

    test tab_search

--- classes/Competence.php
+++ classes/Competence.php
@@ -2,6 +2,24 @@
 
 include_once "Database.php";
 
+function tab_search($id, $tab) {
+
+    $comp = array();
+
+    foreach($tab as $c) {
+
+        if ($c["id_competence_mere"] == $id) {
+
+            $comp[$c["nom"]] = tab_search($c["id"], $tab);
+
+        }
+
+    }
+
+    return $comp;
+
+}
+
 class Competence {
 
     public function get_array($id = null) {
@@ -17,15 +35,16 @@ class Competence {
             $answer = $statement->fetchAll(PDO::FETCH_ASSOC);
             
             foreach($answer as $c) {
+
                 if ($c["id_competence_mere"] == null) {
 
-                    $comp[$c["nom"]] = array(
-                        "id_competence" => $c["id_competence"],
-                        "id_competence_mere" => $c["id_competence_mere"]
-                    );
+                    $comp[$c["nom"]] = tab_search($c["id"], $tab);
 
                 }
+
             }
+
+            foreach($comp)
 
             return $comp;
         
