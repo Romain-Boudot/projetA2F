f64f6276957250cd7f1ebac56c61e347c77cc5f3
Unknown <romain.boudot@epsi.fr>
2018-05-21 15:13:21 +0200

    maket consultant fini

--- html/test.php
+++ html/test.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="main.css">
-    <script src="/cdn/test.js"></script>
+    <script src="/cdn/Dropdown.js"></script>
     <title>test</title>
 </head>
 <body>
@@ -13,27 +13,22 @@
     <div class="mainContainer">
 
         <div id="ot1" class="ongletTrigger">trigger 1</div>
-        <div id="ob1" class="ongletBack">
         <div id="oc1" class="ongletContainer">
 
             <span>menu 1</span>
             <div id="ddt2" class="dropdownTrigger">trigger 2</div>
-            <div id="ddb2" class="dropdownBack">
             <div id="ddc2" class="dropdownContainer">
 
                 <span>menu 2</span>
                 <div id="ddt3" class="dropdownTrigger">trigger 3</div>
-                <div id="ddb3" class="dropdownBack">
                 <div id="ddc3" class="dropdownContainer">
 
                     <span>menu 3</span><br>
                     <span>menu 3</span>
                 
                 </div>
-                </div>
 
                 <div id="ddt4" class="dropdownTrigger">trigger 4</div>
-                <div id="ddb4" class="dropdownBack">
                 <div id="ddc4" class="dropdownContainer">
         
                     <span>menu 4</span><br>
@@ -41,50 +36,39 @@
                     <span>menu 4</span>
         
                 </div>
-                </div>
 
-            </div>
             </div>
 
             <div id="ddt5" class="dropdownTrigger">trigger 5</div>
-            <div id="ddb5" class="dropdownBack">
             <div id="ddc5" class="dropdownContainer">
         
                 <span>menu 5</span><br>
                 <span>menu 5</span>
             
             </div>
-            </div>
 
-        </div>
         </div>
 
         <div id="ot6" class="ongletTrigger">trigger 6</div>
-        <div id="ob6" class="ongletBack">
         <div id="oc6" class="ongletContainer">
 
             <span>menu 6</span>
             <div id="ddt7" class="dropdownTrigger">trigger 7</div>
-            <div id="ddb7" class="dropdownBack">
             <div id="ddc7" class="dropdownContainer">
 
                 <span>menu 7</span><br>
                 <span>menu 7</span>
 
             </div>
-            </div>
 
             <div id="ddt8" class="dropdownTrigger">trigger 8</div>
-            <div id="ddb8" class="dropdownBack">
             <div id="ddc8" class="dropdownContainer">
 
                 <span>menu 8</span><br>
                 <span>menu 8</span>
 
             </div>
-            </div>
 
-        </div>
         </div>
 
     </div>
