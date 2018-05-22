52f07d3d777e752a28966ae795d733d9831d5b33
Romain <romain.boudot@epsi.fr>
2018-05-22 11:59:00 +0200

    fix

---
+++ html/recherche/recherche.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="/cdn/main.css">
+    <link rel="stylesheet" href="main.css">
+    <title>Recherche</title>
+</head>
+<body>
+    
+    <header>
+        <div class="header-left">
+            <img id="logo-a2f" src="/images/logo-a2f-blanc-02.svg" height="46">
+        </div>
+        <div class="header-right">
+            <div class="btn bold">Déconnexion</div>
+        </div>
+        <div class="header-right">
+            <div>Bienvenue, <span>romain.boudot</span></div>
+        </div>
+    </header>
+
+    <div class="main-wrapper">
+
+        <div class="search">
+            
+            <input type="text" class="searchBar" placeholder="nom consultant">
+            
+            
+
+        </div>
+        
+    </div>
+
+</body>
+</html>
