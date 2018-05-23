1c1723c16fc9e599f5e6bd12bcdf301cdb0b99e5
Unknown <elian.bourdu@gmail.com>
2018-05-23 11:10:38 +0200

    intervention consultant

--- html/consultant/consultant.php
+++ html/consultant/consultant.php
@@ -82,22 +82,32 @@
                             </div>
                         </div>
                     </div>
+
+                    <?php
+
+                        $tab = $consultant->get_intervention();
+
+                        foreach ($tab as $int) {
+                            ?>
+
                     <div class="hr"></div>
                     <div class="intervention">
-                        <div class="infos">17 mai 2018</div>
-                        <div class="infos">A2F advisor</div>
-                        <div class="details">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.                                                            
-                        </div>
+                        <div class="infos"><?php $int['i.date']; ?></div>
+                        <div class="infos"><?php $int['c.entreprise']; ?></div>
+                        <div class="details"><?php $int['i.details']; ?></div>
                     </div>
-                    <div class="hr"></div>
-                    <div class="intervention">
-                        <div class="infos">17 mai 2018</div>
-                        <div class="infos">A2F advisor</div>
-                        <div class="details">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
-                        </div>
-                    </div>
+
+                            <?php
+                        }
+                    ?>
+                
+                     <?php
+
+                        //$tab = $consultant->get_qualifications();
+
+                        //foreach ($tab as $int) {
+                            ?>
+
                 </div>
                 <div id="oc2" class="onglet ongletContainer">
                     <div class="qualification">
@@ -110,6 +120,8 @@
                             </div>
                         </div>
                     </div>
+
+
                     <div class="hr"></div>
                     <div class="qualification">
                         <div class="infos">
@@ -122,6 +134,8 @@
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.                                
                         </div>
                     </div>
+
+
                     <div class="hr"></div>
                     <div class="qualification">
                         <div class="infos">
@@ -134,6 +148,7 @@
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.                                
                         </div>
                     </div>
+
                     <div class="hr"></div>
                     <div class="qualification">
                         <div class="infos">
