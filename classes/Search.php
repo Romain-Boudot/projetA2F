2b3bebf04920be04fba71ced72fb71cc45862715
Sylouan Corfa <T4raal@gmail.com>
2018-05-24 17:46:11 +0200

    SEARCH COMPETENCE FU

--- classes/Search.php
+++ classes/Search.php
@@ -1,51 +1,71 @@
 <?php
+error_reporting(E_ALL);
+include 'Database.php';
 
 Class Search {
 
-    public static function search(){
-        $pdo = Database::connect();
-        $where = false;
+    static public function lookup(){
+        $array = json_decode($_GET["filter"], true);
+    
+
+            $pdo = Database::connect();
+        $where = 0;
         $i = 0;
+        $i2 = 0;
+        $bindparamcpt = 0;
+        $bindparam = array();
+
+        $statement = "SELECT * from consultants c JOIN interventions i ON i.id_consultant = c.id_consultant JOIN clients cl ON cl.id_client=i.id_client ";
 
 
-        $statement = "SELECT * from consultants c JOIN interventions i ON i.id_consultant = c.id_consultant JOIN client cl ON cl.id_client=i.id_client ";
-
-
-        if(isset($_GET['competence'])){
-            $statement .= " JOIN competences_consultants cc ON cc.id_consultant = c.id_consultant JOIN competences comp ON comp.id_competence = cc.id_competence "
+        if(isset($array['competence'])){
+            $statement .= " JOIN competences_consultants cc ON cc.id_consultant = c.id_consultant JOIN competences comp ON comp.id_competence = cc.id_competence ";
 
         }
 
-        if(isset($_GET['disponibilite'])){
+        if(isset($array['disponibilite'])){
 
             $statement .= " JOIN disponibilite_consultant dc ON dc.id_consultant = c.id_consultant ";
 
         }
 
-        if(isset($_GET){
-            $statement .= " WHERE ";
+        if(isset($array['competences'])){
+            
+            if ($where == 0) {
+                $statement .= " WHERE ";
+                $where = 1;
+            }
 
-            foreach($_GET as $name => $value){
-                
-                if ($i != 0){
-                    $statement .= " AND ";
-                } 
-               
-                $statement .= " :getname = :value "
+            foreach($array["competences"]["id_competence"] as $key => $value){
+
+                if ($where != 0) {
+                    if ($where == 1) {
+                        $where = 2;
+                    } else {
+                        $statement .= " AND ";
+                    }
+                }
+
+                $statement .= " (cc.id_competence = :bp" . $bindparamcpt . " AND cc.niveau = :bp" . ($bindparamcpt + 1) . " ) ";
+
+                $bindparam[":bp" . $bindparamcpt] = $value;
+                $bindparam[":bp" . ($bindparamcpt + 1)] = $array["competences"]["niveau"][$key];
+                $bindparamcpt += 2;
 
             }
 
-            
-
-
         }
+        var_dump($statement);
+ //       $query->fetchAll();
+
+        var_dump($bindparam);
+//$query->debugDumpParams();
+//        return $query;
+    } 
+
+}
 
 
-    }
 
 
 
-
-
-
-?>
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+include 'classes/Search.php';
+
+Search::lookup();
+
+
+?>
