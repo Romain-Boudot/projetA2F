22e3400c3174d35f9e99570fc467b87e7581843b
Romain <romain.boudot@epsi.fr>
2018-05-25 17:18:54 +0200

    search

--- html/cdn/Popup.js
+++ html/cdn/Popup.js
@@ -1,14 +1,26 @@
 class Popup {
 
-    static open(id) {
+    static open(id, ...triggers) {
 
         document.getElementById(id).style.visibility = "visible";
 
+        triggers.forEach(e => {
+            if (e.onPopupOpen) {
+                e.onPopupOpen();
+            }
+        })
+
     }
 
-    static close(id) {
+    static close(id, ...triggers) {
 
         document.getElementById(id).style.visibility = "hidden";
+
+        triggers.forEach(e => {
+            if (e.onPopupClose) {
+                e.onPopupClose();
+            }
+        })
 
     }
 
--- html/recherche/recherche.php
+++ html/recherche/recherche.php
@@ -13,123 +13,123 @@ $_SESSION["user"] = array(
 )
 
 ?>
-    <!DOCTYPE html>
-    <html lang="fr">
+<!DOCTYPE html>
+<html lang="fr">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link rel="stylesheet" href="/cdn/main.css">
-        <link rel="stylesheet" href="main.css">
-        <script src="/cdn/Popup.js"></script>
-        <script src="/cdn/Dropdown.js"></script>
-        <script src="/cdn/Search.js"></script>
-        <title>Recherche</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="/cdn/main.css">
+    <link rel="stylesheet" href="main.css">
+    <script src="/cdn/Popup.js"></script>
+    <script src="/cdn/Dropdown.js"></script>
+    <script src="/cdn/Search.js"></script>
+    <title>Recherche</title>
+</head>
 
-    <body>
+<body>
 
-        <header>
-            <div class="header-left">
-                <img id="logo-a2f" src="/images/logo-a2f-blanc-02.svg" height="46">
+    <header>
+        <div class="header-left">
+            <img id="logo-a2f" src="/images/logo-a2f-blanc-02.svg" height="46">
+        </div>
+        <div class="header-right">
+            <div class="btn bold">Déconnexion</div>
+        </div>
+        <div class="header-right">
+            <div>Bienvenue,
+                <span>romain.boudot</span>
             </div>
-            <div class="header-right">
-                <div class="btn bold">Déconnexion</div>
-            </div>
-            <div class="header-right">
-                <div>Bienvenue,
-                    <span>romain.boudot</span>
-                </div>
-            </div>
-        </header>
+        </div>
+    </header>
 
-        <div class="main-wrapper">
+    <div class="main-wrapper">
 
-            <div class="search">
-                <input type="text" class="searchBar" placeholder="Entrez un nom (optionnel)">
+        <div class="search">
+            <input type="text" class="searchBar" placeholder="Entrez un nom (optionnel)">
 
-                <?php
+            <?php
 
-                    if ($_SESSION['user']['type'] == 2) {
+                if ($_SESSION['user']['type'] == 2) {
 
-                ?>
+            ?>
 
-                    <label for="archive">
-                        <input type="checkbox" name="archive" value="archive" id="archive">
-                        <div for="archive" class="checkbox">✔</div>
-                        archive
+            <label for="archive">
+                <input type="checkbox" name="archive" id="archive">
+                <div for="archive" class="checkbox">✔</div>
+                archive
+            </label>
+
+            <?php
+
+                }
+
+            ?>
+
+            <div class="filterGrid">
+
+                <div class="filterGridLeft borderRight">
+
+                    Pôles :
+                    <br>
+                    <label for="poleIndus" class="pole">
+                        <input type="checkbox" name="poleIndus" id="poleIndus">
+                        <div class="checkbox">✔</div>
+                        Indus
+                    </label>
+                    <label for="poleDatabase" class="pole">
+                        <input type="checkbox" name="poleDatabase" id="poleDatabase">
+                        <div class="checkbox">✔</div>
+                        Database
+                    </label>
+                    <label for="poleSi" class="pole">
+                        <input type="checkbox" name="poleSi" id="poleSi">
+                        <div class="checkbox">✔</div>
+                        Si
                     </label>
 
-                    <?php
+                </div>
 
-                        }
+                <div class="filterGridRight">
+                    Disponibilités :
+                    <label for="dispMtn" class="disp">
+                        <input type="checkbox" name="dispMtn" id="dispMtn">
+                        <div class="checkbox">✔</div>
+                        Maintenant
+                    </label>
+                    <label for="disp1M" class="disp">
+                        <input type="checkbox" name="disp1M" id="disp1M">
+                        <div class="checkbox">✔</div>
+                        Dans 1 mois
+                    </label>
+                    <br>
+                    <label for="disp2M" class="disp">
+                        <input type="checkbox" name="disp2M" id="disp2M">
+                        <div class="checkbox">✔</div>
+                        Dans 2 mois
+                    </label>
+                    <label for="disp3M" class="disp">
+                        <input type="checkbox" name="disp3M" id="disp3M">
+                        <div class="checkbox">✔</div>
+                        Dans 3 mois et plus
+                    </label>
+                </div>
 
-                    ?>
+                <div class="filterGridLarge borderTop divClientList">
 
-                    <div class="filterGrid">
+                    <div class="btn fakeComp" onclick="Popup.open('popupClient')">Selection des clients</div>
 
-                        <div class="filterGridLeft borderRight">
+                </div>
 
-                            Pôles :
-                            <br>
-                            <label for="poleIndus" class="pole">
-                                <input type="checkbox" name="poleIndus" id="poleIndus">
-                                <div class="checkbox">✔</div>
-                                Indus
-                            </label>
-                            <label for="poleDatabase" class="pole">
-                                <input type="checkbox" name="poleDatabase" id="poleDatabase">
-                                <div class="checkbox">✔</div>
-                                Database
-                            </label>
-                            <label for="poleSi" class="pole">
-                                <input type="checkbox" name="poleSi" id="poleSi">
-                                <div class="checkbox">✔</div>
-                                Si
-                            </label>
+                <div class="filterGridLarge borderTop divCompList">
 
-                        </div>
+                    <div class="btn fakeComp" onclick="Popup.open('popupComp')">Selection des competences</div>
 
-                        <div class="filterGridRight">
-                            Disponibilités :
-                            <label for="dispMtn" class="disp">
-                                <input type="checkbox" name="dispMtn" id="dispMtn">
-                                <div class="checkbox">✔</div>
-                                Maintenant
-                            </label>
-                            <label for="disp1M" class="disp">
-                                <input type="checkbox" name="disp1M" id="disp1M">
-                                <div class="checkbox">✔</div>
-                                Dans 1 mois
-                            </label>
-                            <br>
-                            <label for="disp2M" class="disp">
-                                <input type="checkbox" name="disp2M" id="disp2M">
-                                <div class="checkbox">✔</div>
-                                Dans 2 mois
-                            </label>
-                            <label for="disp3M" class="disp">
-                                <input type="checkbox" name="disp3M" id="disp3M">
-                                <div class="checkbox">✔</div>
-                                Dans 3 mois et plus
-                            </label>
-                        </div>
+                </div>
 
-                        <div class="filterGridLarge borderTop divClientList">
-
-                            <div class="btn fakeComp" onclick="Popup.open('popupClient')">Selection des clients</div>
-
-                        </div>
-
-                        <div class="filterGridLarge borderTop divCompList">
-
-                            <div class="btn fakeComp" onclick="Popup.open('popupComp')">Selection des competences</div>
-
-                        </div>
-
-                    </div>
+            </div>
 
         </div>
 
@@ -148,16 +148,14 @@ $_SESSION["user"] = array(
                 // parcour d'un tableau et ressort le nom de la competence et ses enfant ( recursion )
                 function tab($tab, $cpt) {
 
-                    $html = array();
-
                     ?>
                         <div id="ddc<?php echo $cpt?>" class="dropdownContainer">
                     <?php
 
                     $cpt += 1;
-                    
+                
                     foreach ($tab as $name => $value) {
-                        
+                    
                         if ($value["enfant"] != null) {
                             
                             ?>
@@ -166,7 +164,7 @@ $_SESSION["user"] = array(
                                     <?php echo $name; ?>
                                 </div>
 
-                            <?php
+                                <?php
                             
                             $returned = tab($value["enfant"], $cpt);
 
@@ -196,14 +194,16 @@ $_SESSION["user"] = array(
 
                 };
 
+                $cpt = 0;
+
                 foreach ($comp as $name => $value) {
 
                     if ($value["enfant"] != null) {
 
                         ?>
-                        <div id="ddt<?php echo $cpt?>" class="dropdownTrigger">
-                            <?php echo $name; ?>
-                        </div>
+                            <div id="ddt<?php echo $cpt?>" class="dropdownTrigger">
+                                <?php echo $name; ?>
+                            </div>
                         <?php
                         
                         $returned = tab($value["enfant"], $cpt);
@@ -214,7 +214,7 @@ $_SESSION["user"] = array(
 
                 }
 
-            ?>
+                ?>
 
                 <script>
                     Dropdown.load();
@@ -232,7 +232,7 @@ $_SESSION["user"] = array(
 
         </div>
 
-        <div class="compSelectClose btn close" onclick="Popup.close('popupComp')">Valider</div>
+        <div class="compSelectClose btn close" onclick="Popup.close('popupComp', search)">Valider</div>
 
     </div>
 
@@ -244,19 +244,19 @@ $_SESSION["user"] = array(
 
                 <?php
 
-                    $c = Client::get_array();
+                $c = Client::get_array();
 
-                    foreach ($c as $client) {
+                foreach ($c as $client) {
 
-                        ?>
+                    ?>
 
-                        <div data-name="<?php echo $client["entreprise"] ?>" data-id="<?php echo $client["id_client"]; ?>" class="client">
-                            <?php echo $client["entreprise"] ?>
-                        </div>
+                    <div data-name="<?php echo $client["entreprise"] ?>" data-id="<?php echo $client["id_client"]; ?>" class="client">
+                        <?php echo $client["entreprise"] ?>
+                    </div>
 
-                        <?php
-                        
-                    }
+                    <?php
+                    
+                }
 
                 ?>
 
@@ -277,7 +277,7 @@ $_SESSION["user"] = array(
     </div>
 
     <script>
-    var search = new Search();
+        var search = new Search();
     </script>
 
 </body>
