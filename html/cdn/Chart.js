1a7fc6ee52f4757af295e619144ca88c17f086bc
Romain <romain.boudot@epsi.fr>
2018-05-29 14:00:04 +0200

    recherche fini

--- html/cdn/Chart.js
+++ html/cdn/Chart.js
@@ -2,23 +2,21 @@ class chartOption {
 
     constructor() {
         this.chart = {
-            "type":"radar",
+            "type": "radar",
             "data": {
                 "labels": [],
-                "datasets":[
-                    {
-                        "data": [],
-                        "tension": 0,
-                        "backgroundColor": "#4582cf88",
-                        "borderColor": "#2f336f",
-                        "pointBackgroundColor": "#2f336f",
-                        "pointBorderColor": "#fff",
-                        "pointHoverBackgroundColor": "#fff",
-                        "pointHoverBorderColor": "#2f336f"
-                    }
-                ]
+                "datasets": [{
+                    "data": [],
+                    "tension": 0,
+                    "backgroundColor": "#4582cf88",
+                    "borderColor": "#2f336f",
+                    "pointBackgroundColor": "#2f336f",
+                    "pointBorderColor": "#fff",
+                    "pointHoverBackgroundColor": "#fff",
+                    "pointHoverBorderColor": "#2f336f"
+                }]
             },
-            "options":{
+            "options": {
                 "scale": {
                     "ticks": {
                         "max": 3,
@@ -26,10 +24,10 @@ class chartOption {
                         "stepSize": 1
                     }
                 },
-                "legend":{
+                "legend": {
                     "display": false
                 },
-                "layout":{
+                "layout": {
                     "padding": {
                         "top": 20,
                         "left": 20,
@@ -37,10 +35,69 @@ class chartOption {
                         "bottom": 20
                     }
                 },
-                "elements":{
-                    "line":{
-                        "borderWidth":3
+                "elements": {
+                    "line": {
+                        "borderWidth": 3
                     }
+                }
+            }
+        }
+    }
+
+    get option() {
+        return this.chart
+    }
+
+}
+
+class chartOptionBaton {
+
+    constructor() {
+
+        Chart.defaults.global.legend.labels.boxWidth = 5
+
+        this.chart = {
+            "type": "bar",
+            "data": {
+                "labels": [],
+                "datasets": [{
+                    "data": [],
+                    "backgroundColor": "#4582cf88",
+                    "borderColor": "#2f336f",
+                    "fill": false,
+                    "hoverBackgroundColor": "#4582cf77",
+                    "borderWidth": 1
+                }]
+            },
+            "options": {
+                "tooltips": {
+                    "enabled": false
+                },
+                "legend": {
+                    "display": false
+                },
+                "layout": {
+                    "padding": {
+                        "top": 20,
+                        "left": 20,
+                        "right": 20,
+                        "bottom": 20
+                    }
+                },
+                "elements": {
+                    "line": {
+                        "borderWidth": 3
+                    }
+                },
+                "scales": {
+                    "yAxes": [{
+                        "ticks": {
+                            "beginAtZero": true
+                        }
+                    }],
+                    "xAxes": [{
+                        "maxBarThickness": 50
+                    }]
                 }
             }
         }
