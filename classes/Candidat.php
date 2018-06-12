572b91c44ca8804512fcc8dd42c60392351e4be4
Unknown <romain.boudot@epsi.fr>
2018-06-12 12:12:30 +0200

    time line candidat

--- classes/Candidat.php
+++ classes/Candidat.php
@@ -247,6 +247,12 @@ class Candidat {
 
     public function set_etape($etape){
         $this->etape = $etape;
+        $db = Database::connect();
+        $statement = $db->prepare("UPDATE candidats SET etape = :etape WHERE id_candidat = :id");
+        $statement->execute(array(
+            ":etape" => $this->etape,
+            ":id" => $this->id
+        ));
     }
 
     public function get_interviews(){
