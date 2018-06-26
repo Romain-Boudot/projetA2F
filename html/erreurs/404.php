c6f6a24528fc6450c021556cdf10f4474af3764d
Unknown <romain.boudot@epsi.fr>
2018-06-26 17:02:22 +0200

    fix couleur + pages d'erreurs

--- html/erreurs/404.php
+++ html/erreurs/404.php
@@ -1,12 +1,67 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <style>
+        html {
+            font-family: "Roboto", sans-serif;
+        }
+        body {
+            background-image: url("/images/pattern-a2f.png");
+            background-size: 100% 100%;
+            background-repeat: no-repeat;
+            margin: 0;
+            height: 100vh;
+        }
+        body * {
+            position: relative;
+            display: block;
+        }
+        h0 {
+            font-size: 60px;
+        }
+        .title,
+        .content {
+            width: 100vw;
+            text-align: center;
+        }
+        .content {
+            margin-top: 100px;
+            font-size: 30px;
+        }
+        a {
+            border: 2px solid rgba(0, 0, 0, .2);
+            background-color: #06436f;
+            padding: 10px 15px;
+            margin-top: 50px;
+            text-decoration: none;
+            color: white;
+            display: inline-block;
+            border-radius: 7px;
+        }
+        img {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+        }
+    </style>
     <title>Page introuvable</title>
 </head>
 <body>
-    page introuvable
+
+    <span>Erreur 404</span>
+    <img src="/images/logo-a2f-footer.svg" alt="logo a2f" height="70">
+    <div class="title">
+        <h0>Page introuvable !</h0>
+    </div>
+
+    <div class="content">
+        <span>La page à laquelle vous essayez d'accéder n'a pas été trouvée...</span>
+        <a href="/">Retourner sur le site</a>
+    </div>
+
 </body>
 </html>
