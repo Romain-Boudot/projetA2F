5997e95d3a99967db397562bf162d8a263ece31a
Romain <romain.boudot@epsi.fr>
2018-06-26 14:22:40 +0200

    ajout de text sur transfer

--- html/candidat/transfer.php
+++ html/candidat/transfer.php
@@ -30,19 +30,22 @@ if (!isset($_GET["ajaxtrue"])) {
     .imgPole {
         display: inline-block;
         margin: 10px 20px;
-        border: 2px solid rgba(0, 0, 0, .1);
         padding: 10px;
-        border-radius: 7px;
+        cursor: pointer;
+    }
+    .imgPole>img {
         filter: grayscale(1);
         -webkit-filter: grayscale(1);
         -moz-filter: grayscale(1);
         -o-filter: grayscale(1);
         -ms-filter: grayscale(1);
+        border-radius: 7px;
         transition: .2s filter ease, .2s box-shadow ease;
-        cursor: pointer;
+        padding: 10px;
+        border: 2px solid rgba(0, 0, 0, .1);
     }
-    .imgPole:hover,
-    .active {
+    .imgPole:hover>img,
+    .active>img {
         filter: grayscale(0);
         -webkit-filter: grayscale(0);
         -moz-filter: grayscale(0);
@@ -58,6 +61,7 @@ if (!isset($_GET["ajaxtrue"])) {
         transition: .2s background-color ease, .2s color ease;
         cursor: pointer;
         margin-right: 20px;
+        margin-top: 20px;
     }
     .btnTransfert:hover {
         background-color: var(--main-color);
@@ -71,15 +75,18 @@ if (!isset($_GET["ajaxtrue"])) {
     </div>
 
     <div onclick="Transfer.Indus()" class="imgPole" id="Indus">
-        <img src="/images/schema-industrie-15.svg" alt="logo a2f industrie" height="100" width="100">
+        <img src="/images/schema-industrie-15.svg" alt="logo a2f industrie" height="100" width="100"><br>
+        <label for="Indus">Indus</label>
     </div>
 
     <div onclick="Transfer.SI()" class="imgPole" id="SI">
-        <img src="/images/visuel-si.svg" alt="logo a2f SI" height="100" width="100">
+        <img src="/images/visuel-si.svg" alt="logo a2f SI" height="100" width="100"><br>
+        <label for="SI">SI</label>
     </div>
 
     <div onclick="Transfer.Database()" class="imgPole" id="Database">
-        <img src="/images/schema-database-17.svg" alt="logo a2f database" height="100" width="100">
+        <img src="/images/schema-database-17.svg" alt="logo a2f database" height="100" width="100"><br>
+        <label for="Database">Database</label>
     </div>
 
     <br>
